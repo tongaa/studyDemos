77f3d0e5419198ff7bb7cefa04675108e9e3b69a
tongaa <tgw0624@163.com>
2019-08-20 15:54:53 +0800

    委托带来了事件

---
+++ 委托实例/ConsoleApp1/事件/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cat cat = new Cat();
+
+        cat.catEvent += Mouse;//订阅猫事件
+        cat.catEvent += Person;//订阅人事件
+
+        cat.Cry("猫：喵~");//猫叫的动作，触发事件
+
+        Console.ReadKey();
+    }
+
+    private static void Person(object sender, EventArgs e)
+    {
+        Console.WriteLine("人：大半夜的不睡觉，叫唤个啥呢？~");
+    }
+
+    private static void Mouse(object sender, EventArgs e)
+    {
+        Console.WriteLine("老鼠：快跑~");
+    }
+}
+
+public class Cat
+{
+    public event EventHandler<EventArgs> catEvent;
+
+    public void Cry(string msg)
+    {
+        Console.WriteLine(msg);
+
+        catEvent(this, new EventArgs()); //开始执行订阅的事件 先老鼠后人
+    }
+}
---
+++ 委托实例/ConsoleApp1/委托/Trust.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public delegate void MyDelegate(string s); //创建委托实例
+    class Trust
+    {
+        static void TestMetthod1( string str)
+        {
+            Console.WriteLine("这是方法1：{0}",str);
+        }
+        static void TestMetthod2(string str)
+        {
+            Console.WriteLine("这是方法2：{0}", str);
+        }
+        static void TestMetthod3(string str)
+        {
+            Console.WriteLine("这是方法3：{0}", str);
+        }
+
+        //将方法作为参数传递 委托可以让方法作为参数传递给其它方法。
+        static void Test(MyDelegate d)
+        {
+            if (d != null)
+            {
+                d("5");
+                Console.WriteLine("123");
+            }
+
+        }
+           
+
+
+        static void Main(string[] args)
+        {
+            MyDelegate D1, D2; //定义委托变量
+            MyDelegate D4; //定义委托变量
+            MyDelegate D5; //定义委托变量
+
+            D1 = TestMetthod1; //关联方法
+                               //  D1("111"); //调用委托实例
+
+            //一个委托实例关联三个方法
+            D4 = TestMetthod1;
+            D4 += TestMetthod2;
+            D4 += TestMetthod3;
+          //  D4("4"); //三个方法均调用一次
+           // 移除一个委托实例中的方法 使用“-”
+            D4 -= TestMetthod2;
+           // D4("4"); //2个方法均调用一次 去掉 TestMetthod2
+            D5 = TestMetthod1;
+            Test(D5);
+
+
+
+        }
+    }
+}
