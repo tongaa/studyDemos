fe6934794031b690cf7f44ab809d68669968f582
tongaa <tgw0624@163.com>
2019-08-29 16:44:20 +0800

    反射入门

---
+++ 反射入门/ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 这是 demo用的类库
+namespace ClassLibrary1
+{
+    public class People
+    {
+        private string name = null;
+        public People() { }
+        public People(string strName)
+        {
+            name = strName;
+        }
+        public void Say()
+        {
+            if (name == null)
+            {
+                Console.WriteLine("hi~~");
+            }
+            Console.WriteLine("hi" + name);
+        }
+    }
+    public class Man
+    {
+        void Eat() { Console.WriteLine("EAT"); }
+    }
+}
---
+++ 反射入门/ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type people = null;
+            //注意：执行这句代码时要将ClassPeople.dll复制到Debug目录下，这样才能找到这个文件
+            Assembly ass = Assembly.LoadFrom("ClassLibrary1.dll");
+            Type[] mytypes = ass.GetTypes();
+            Console.WriteLine("列出程序集中的所有类型");
+            foreach (Type t in mytypes)
+            {
+                Console.WriteLine(t.Name);
+                if (t.Name == "People")
+                {
+                    people = t;
+                }
+            }
+            Console.WriteLine("列出People类中的方法：");
+            MethodInfo[] methodInfo = people.GetMethods();
+            foreach (MethodInfo mi in methodInfo)
+            {
+                Console.WriteLine(mi.Name);
+            }
+            //实例化People，并调用Say方法
+            Object obj = Activator.CreateInstance(people);
+            Object objName = Activator.CreateInstance(people, "kobe");
+            MethodInfo minfo = people.GetMethod("Say");
+            Console.WriteLine("调用Say方法");
+            minfo.Invoke(obj, null);
+            minfo.Invoke(objName, null);
+            Console.ReadLine();
+        }
+    }
+}
